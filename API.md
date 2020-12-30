b53b9e69d1c6426abf8404951c6a23a28ea35522
Thon Becker <thon.becker@gmail.com>
2020-12-30 03:01:31 +0000

    Update API doc

--- API.md
+++ API.md
@@ -71,7 +71,7 @@ new CreateCloudfrontSite(scope: Construct, id: string, props: CloudfrontSiteConf
   * **websiteFolder** (<code>string</code>)  local path to the website folder you want to deploy on S3. 
   * **encryptBucket** (<code>boolean</code>)  enable encryption for files in your S3 Bucket. __*Optional*__
   * **errorDoc** (<code>string</code>)  the error document of your CloudFront distribution. __*Optional*__
-  * **priceClass** (<code>[PriceClass](#aws-cdk-aws-cloudfront-priceclass)</code>)  The price class determines how many edge locations CloudFront will use for your distribution. __*Optional*__
+  * **priceClass** (<code>[PriceClass](#aws-cdk-aws-cloudfront-priceclass)</code>)  the price class determines how many edge locations CloudFront will use for your distribution. __*Optional*__
 
 
 
@@ -109,7 +109,7 @@ Name | Type | Description
 **websiteFolder** | <code>string</code> | local path to the website folder you want to deploy on S3.
 **encryptBucket**? | <code>boolean</code> | enable encryption for files in your S3 Bucket.<br/>__*Optional*__
 **errorDoc**? | <code>string</code> | the error document of your CloudFront distribution.<br/>__*Optional*__
-**priceClass**? | <code>[PriceClass](#aws-cdk-aws-cloudfront-priceclass)</code> | The price class determines how many edge locations CloudFront will use for your distribution.<br/>__*Optional*__
+**priceClass**? | <code>[PriceClass](#aws-cdk-aws-cloudfront-priceclass)</code> | the price class determines how many edge locations CloudFront will use for your distribution.<br/>__*Optional*__
 
 
 
--- src/cdk-simplewebsite-deploy.ts
+++ src/cdk-simplewebsite-deploy.ts
@@ -1,11 +1,11 @@
-import * as acm from "@aws-cdk/aws-certificatemanager";
-import * as cloudfront from "@aws-cdk/aws-cloudfront";
-import * as origins from "@aws-cdk/aws-cloudfront-origins";
-import * as route53 from "@aws-cdk/aws-route53";
-import * as targets from "@aws-cdk/aws-route53-targets";
-import * as s3 from "@aws-cdk/aws-s3";
-import * as s3deploy from "@aws-cdk/aws-s3-deployment";
-import * as cdk from "@aws-cdk/core";
+import * as acm from '@aws-cdk/aws-certificatemanager';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
+import * as route53 from '@aws-cdk/aws-route53';
+import * as targets from '@aws-cdk/aws-route53-targets';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as s3deploy from '@aws-cdk/aws-s3-deployment';
+import * as cdk from '@aws-cdk/core';
 
 export interface BasicSiteConfiguration {
   /**
@@ -72,7 +72,7 @@ export class CreateBasicSite extends cdk.Construct {
     super(scope, id);
 
     if (props.websiteDomain && props.websiteSubDomain) {
-      new s3.Bucket(scope, "WebsiteRedirectBucket", {
+      new s3.Bucket(scope, 'WebsiteRedirectBucket', {
         bucketName: props.websiteSubDomain,
         websiteRedirect: {
           hostName: props.websiteDomain,
@@ -81,7 +81,7 @@ export class CreateBasicSite extends cdk.Construct {
       });
     }
 
-    const websiteBucket = new s3.Bucket(scope, "WebsiteBucket", {
+    const websiteBucket = new s3.Bucket(scope, 'WebsiteBucket', {
       bucketName: props.websiteDomain,
       websiteIndexDocument: props.indexDoc,
       websiteErrorDocument: props.errorDoc,
@@ -91,7 +91,7 @@ export class CreateBasicSite extends cdk.Construct {
         : s3.BucketEncryption.UNENCRYPTED,
     });
 
-    new s3deploy.BucketDeployment(scope, "WebsiteDeploy", {
+    new s3deploy.BucketDeployment(scope, 'WebsiteDeploy', {
       sources: [s3deploy.Source.asset(props.websiteFolder)],
       destinationBucket: websiteBucket,
     });
@@ -102,32 +102,32 @@ export class CreateCloudfrontSite extends cdk.Construct {
   constructor(
     scope: cdk.Construct,
     id: string,
-    props: CloudfrontSiteConfiguration
+    props: CloudfrontSiteConfiguration,
   ) {
     super(scope, id);
 
     const hostedZone = route53.HostedZone.fromLookup(
       this,
-      "WebsiteHostedZone",
+      'WebsiteHostedZone',
       {
         domainName: props.hostedZoneDomain,
-      }
+      },
     );
 
-    const websiteCert = new acm.DnsValidatedCertificate(this, "WebsiteCert", {
+    const websiteCert = new acm.DnsValidatedCertificate(this, 'WebsiteCert', {
       domainName: props.websiteDomain,
       hostedZone: hostedZone,
-      region: "us-east-1",
+      region: 'us-east-1',
     });
 
-    const websiteBucket = new s3.Bucket(scope, "WebsiteBucket", {
+    const websiteBucket = new s3.Bucket(scope, 'WebsiteBucket', {
       publicReadAccess: false,
       encryption: props.encryptBucket
         ? s3.BucketEncryption.S3_MANAGED
         : s3.BucketEncryption.UNENCRYPTED,
     });
 
-    const websiteDist = new cloudfront.Distribution(this, "WebsiteDist", {
+    const websiteDist = new cloudfront.Distribution(this, 'WebsiteDist', {
       defaultBehavior: {
         origin: new origins.S3Origin(websiteBucket),
         allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
@@ -158,18 +158,18 @@ export class CreateCloudfrontSite extends cdk.Construct {
       certificate: websiteCert,
     });
 
-    new s3deploy.BucketDeployment(scope, "WebsiteDeploy", {
+    new s3deploy.BucketDeployment(scope, 'WebsiteDeploy', {
       sources: [s3deploy.Source.asset(props.websiteFolder)],
       destinationBucket: websiteBucket,
       distribution: websiteDist,
-      distributionPaths: ["/", `/${props.indexDoc}`],
+      distributionPaths: ['/', `/${props.indexDoc}`],
     });
 
-    new route53.ARecord(this, "WebisteAlias", {
+    new route53.ARecord(this, 'WebisteAlias', {
       zone: hostedZone,
       recordName: props.websiteDomain,
       target: route53.RecordTarget.fromAlias(
-        new targets.CloudFrontTarget(websiteDist)
+        new targets.CloudFrontTarget(websiteDist),
       ),
     });
   }
