7edec75f90c1d184dc907c7dc398d9d8c28c9fee
Thon Becker <thon.becker@gmail.com>
2021-05-27 10:15:00 -0500

    chore(deps): upgrade packages

--- .projenrc.js
+++ .projenrc.js
@@ -4,7 +4,7 @@ const project = new AwsCdkConstructLibrary({
   authorAddress: 'thon.becker@gmail.com',
   authorName: 'Thon Becker',
   stability: 'stable',
-  cdkVersion: '1.106.0',
+  cdkVersion: '1.106.1',
   name: 'cdk-simplewebsite-deploy',
   repository: 'https://github.com/SnapPetal/cdk-simplewebsite-deploy',
   cdkAssert: true,
