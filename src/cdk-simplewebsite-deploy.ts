bdddb6ddfc2a138a283efccdd23c0c5400f3f175
EC2 Default User <ec2-user@ip-172-31-29-46.ec2.internal>
2022-01-01 01:44:51 +0000

    fix: adjust import to remove error on core

--- src/cdk-simplewebsite-deploy.ts
+++ src/cdk-simplewebsite-deploy.ts
@@ -1,3 +1,4 @@
+import { RemovalPolicy } from 'aws-cdk-lib';
 import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
@@ -5,7 +6,6 @@ import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as targets from 'aws-cdk-lib/aws-route53-targets';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
-import { RemovalPolicy } from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 
 export declare enum PriceClass {
